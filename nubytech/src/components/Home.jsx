8af019c38639f5df35db5b0432abb6f075008cca
Austin <austinematurure@gmail.com>
2024-08-20 13:38:34 +0300

    Initial Commit

---
+++ nubytech/src/components/Home.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import "../css/home.css";
+
+function Home() {
+  return <></>;
+}
+
+export default Home;
---
+++ nubytech/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "../css/navbar.css";
+
+function Navbar() {
+  return (
+    <>
+      <header>
+        <div className="logo">
+          <h1>NubyTech</h1>
+          <nav></nav>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Navbar;
